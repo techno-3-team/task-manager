95d8a4a1bb4fcac2224353d1908ff087eecfd6a4
Queenore <atcheschev@yandex.ru>
2023-03-05 22:26:19 +0300

    add main screen toolbar

--- app/src/main/java/com/techno_3_team/task_manager/MainActivity.kt
+++ app/src/main/java/com/techno_3_team/task_manager/MainActivity.kt
@@ -1,11 +1,59 @@
 package com.techno_3_team.task_manager
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.techno_3_team.task_manager.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val toolbar: Toolbar = findViewById(binding.toolbar.id)
+        setSupportActionBar(toolbar)
+
+        // кнопка "назад" стилизованная под бургер
+        // не уверен, что это хорошая идея
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.baseline_menu)
+        supportActionBar?.title = ""
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.clear_checked -> {
+                clearCheckedTasks()
+                true
+            }
+            R.id.sort_by_date, R.id.sort_by_name, R.id.sort_by_importance -> {
+                updateTasksOrder(item)
+                item.isChecked = true
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun updateTasksOrder(item: MenuItem) {
+//        TODO()
+    }
+
+    private fun clearCheckedTasks() {
+//        TODO()
     }
 }
