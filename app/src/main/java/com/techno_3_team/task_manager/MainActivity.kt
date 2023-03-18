e800d450dd8033642bf7fdaf41fa786254fe2e5e
Queenore <atcheschev@yandex.ru>
2023-03-18 20:51:58 +0300

    sort popup

--- app/src/main/java/com/techno_3_team/task_manager/MainActivity.kt
+++ app/src/main/java/com/techno_3_team/task_manager/MainActivity.kt
@@ -7,6 +7,7 @@ import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.techno_3_team.task_manager.databinding.ActivityMainBinding
+import com.techno_3_team.task_manager.tasks.SortOrder
 import com.techno_3_team.task_manager.tasks.Task
 import java.util.*
 import kotlin.collections.ArrayList
@@ -15,6 +16,7 @@ import kotlin.collections.ArrayList
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var sortOrder = SortOrder.BY_DATE
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -64,15 +66,33 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.clear_checked -> {
-//                clearCheckedTasks()
+                clearCheckedTasks()
                 true
             }
-            R.id.sort_by_date, R.id.sort_by_name, R.id.sort_by_importance -> {
-//                updateTasksOrder(item)
-                item.isChecked = true
+            R.id.sort_by_date -> {
+                updateTasksOrder()
+                sortOrder = SortOrder.BY_DATE
+                true
+            }
+            R.id.sort_by_name -> {
+                updateTasksOrder()
+                sortOrder = SortOrder.BY_NAME
+                true
+            }
+            R.id.sort_by_importance -> {
+                updateTasksOrder()
+                sortOrder = SortOrder.BY_IMPORTANCE
                 true
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun clearCheckedTasks() {
+        // TODO()
+    }
+
+    private fun updateTasksOrder() {
+        // TODO()
     }
 }
---
+++ app/src/main/java/com/techno_3_team/task_manager/tasks/SortOrder.kt
@@ -0,0 +1,7 @@
+package com.techno_3_team.task_manager.tasks
+
+enum class SortOrder {
+    BY_DATE,
+    BY_NAME,
+    BY_IMPORTANCE
+}
