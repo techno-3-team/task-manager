33a3c935f580ff3b28c30953cbcfedf6236bece2
Quuenore <atcheschev@yandex.ru>
2023-05-12 12:03:44 +0300

    db task spinner done

--- app/src/main/java/com/techno_3_team/task_manager/fragments/TaskFragment.kt
+++ app/src/main/java/com/techno_3_team/task_manager/fragments/TaskFragment.kt
@@ -19,8 +19,10 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.preference.PreferenceManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.techno_3_team.task_manager.*
+import com.techno_3_team.task_manager.adapters.SpinAdapter
 import com.techno_3_team.task_manager.adapters.SubtaskAdapter
 import com.techno_3_team.task_manager.data.LTSTViewModel
+import com.techno_3_team.task_manager.data.entities.List
 import com.techno_3_team.task_manager.data.entities.Subtask
 import com.techno_3_team.task_manager.data.entities.Task
 import com.techno_3_team.task_manager.databinding.TaskFragmentBinding
@@ -31,15 +33,17 @@ import com.techno_3_team.task_manager.support.CURRENT_LIST_ID
 import com.techno_3_team.task_manager.support.SpacingItemDecorator
 import com.techno_3_team.task_manager.support.observeOnce
 import java.util.*
+import java.util.stream.Collectors
 
 
 class TaskFragment() : Fragment(), DatePickerDialog.OnDateSetListener,
     TimePickerDialog.OnTimeSetListener, HasCustomTitle, HasDeleteAction,
-    SubtaskAdapter.TaskFragmentAdapterCallback, AdapterView.OnItemSelectedListener {
+    SubtaskAdapter.TaskFragmentAdapterCallback {
 
     private lateinit var binding: TaskFragmentBinding
     private lateinit var ltstViewModel: LTSTViewModel
     private var taskId: Int = -1
+    private var task: Task? = null
 
     private var day = 0
     private var month = 0
@@ -80,23 +84,26 @@ class TaskFragment() : Fragment(), DatePickerDialog.OnDateSetListener,
         with(binding) {
             ltstViewModel.getTask(taskId).observeOnce(viewLifecycleOwner) {
                 Log.println(Log.INFO, "observe", "task was observed by taskId = $taskId\n$it")
+
                 if (it.isNotEmpty()) {
                     submitButton.visibility = INVISIBLE
-                    val task = it.first()
-                    taskCheck.isChecked = task.isCompleted
-                    editText.setText(task.header)
-                    taDesc.setText(task.description)
+                    task = it.first()
+                    taskCheck.isChecked = task!!.isCompleted
+                    editText.setText(task!!.header)
+                    taDesc.setText(task!!.description)
                     updateIsCheckedState(taskCheck.isChecked)
                 } else {
                     FAB.visibility = INVISIBLE
                 }
-                editText.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
+
+                editText.setOnKeyListener(View.OnKeyListener { _, keyCode, event ->
                     if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
                         editText.clearFocus()
                         return@OnKeyListener true
                     }
                     false
                 })
+
                 if (editText.text.isEmpty()) {
                     editText.isFocusableInTouchMode = true;
                     editText.requestFocus()
@@ -105,15 +112,39 @@ class TaskFragment() : Fragment(), DatePickerDialog.OnDateSetListener,
                         InputMethodManager.SHOW_IMPLICIT
                     )
                 }
+
                 ltstViewModel.readLists.observeOnce(viewLifecycleOwner) { lists ->
+                    val listsName = lists.stream()
+                        .map { list -> list.listName }
+                        .collect(Collectors.toList()) as ArrayList<String>
                     val spinAdapter =
-                        ArrayAdapter(
+                        SpinAdapter(
                             this@TaskFragment.requireContext(),
                             android.R.layout.simple_spinner_item,
-                            lists
+                            listsName,
+                            lists as ArrayList<List>
                         )
+                    spinAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
                     listSpin.adapter = spinAdapter
-                    // ???
+                    if (task != null) {
+                        listSpin.setSelection(spinAdapter.getPosByListId(task!!.listId))
+                    }
+                    listSpin.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                        override fun onItemSelected(
+                            parent: AdapterView<*>,
+                            view: View,
+                            position: Int,
+                            id: Long
+                        ) {
+                            val listId =
+                                (binding.listSpin.adapter as SpinAdapter).getListId(position)
+                            preference.edit().putInt(CURRENT_LIST_ID, listId).apply()
+                        }
+
+                        override fun onNothingSelected(parent: AdapterView<*>?) {
+                            // Another interface callback
+                        }
+                    }
                 }
             }
 
@@ -290,22 +321,5 @@ class TaskFragment() : Fragment(), DatePickerDialog.OnDateSetListener,
                 )
             )
         }
-    }
-
-    private fun addTaskToAnotherList(listId: Int) {
-        ltstViewModel.getTask(taskId).observeOnce(viewLifecycleOwner) {
-            val task = it as Task
-            ltstViewModel.updateTask(
-                Task(taskId, listId, task.header, task.isCompleted, task.date, task.description)
-            )
-        }
-    }
-
-    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        val listId = (binding.listSpin.adapter.getItemId(position) as com.techno_3_team.task_manager.data.entities.List).listId
-        addTaskToAnotherList(listId)
-    }
-
-    override fun onNothingSelected(parent: AdapterView<*>?) {
     }
 }
